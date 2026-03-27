64e5e6e0915ae1fae26ada1e18e454ec964d2e96
nconsigny <nicolas@ethereum.org>
2026-03-27 17:16:29 +0100

    fix: use default WASM import for webpack asyncWebAssembly compat

--- src/web/libs/eth-prices/eth_prices.js
+++ src/web/libs/eth-prices/eth_prices.js
@@ -1,9 +1,9 @@
 /* @ts-self-types="./eth_prices.d.ts" */
 
-import * as wasm from "./eth_prices_bg.wasm";
-import { __wbg_set_wasm } from "./eth_prices_bg.js";
+import wasm from "./eth_prices_bg.wasm";
+import { __wbg_set_wasm, __wbindgen_start } from "./eth_prices_bg.js";
 __wbg_set_wasm(wasm);
-wasm.__wbindgen_start();
+if (typeof __wbindgen_start === 'function') __wbindgen_start();
 export {
     Quoter, Route, createQuoter
 } from "./eth_prices_bg.js";
