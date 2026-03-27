941b79e314451851f64c80ee13ee4f899041d87f
nconsigny <nicolas@ethereum.org>
2026-03-27 18:35:38 +0100

    fix: async WASM init for webpack asyncWebAssembly compatibility

--- src/web/libs/eth-prices/eth_prices.js
+++ src/web/libs/eth-prices/eth_prices.js
@@ -1,9 +1,22 @@
 /* @ts-self-types="./eth_prices.d.ts" */
 
-import wasm from "./eth_prices_bg.wasm";
-import { __wbg_set_wasm, __wbindgen_init_externref_table } from "./eth_prices_bg.js";
-__wbg_set_wasm(wasm);
-__wbindgen_init_externref_table();
-export {
-    Quoter, Route, createQuoter
-} from "./eth_prices_bg.js";
+import init from "./eth_prices_bg.wasm";
+import * as bg from "./eth_prices_bg.js";
+
+let initialized = false;
+
+async function ensureInit() {
+  if (initialized) return;
+  const wasm = await init({ "./eth_prices_bg.js": bg });
+  bg.__wbg_set_wasm(wasm);
+  bg.__wbindgen_init_externref_table();
+  initialized = true;
+}
+
+export async function createQuoter(config) {
+  await ensureInit();
+  return bg.createQuoter(config);
+}
+
+export const Quoter = bg.Quoter;
+export const Route = bg.Route;
